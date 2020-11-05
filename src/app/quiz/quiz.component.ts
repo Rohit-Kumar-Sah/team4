5a84596f992d90a8fe13fda97701abbe56abb9b6
Rupam Mondal <mondalrupam2014@gmail.com>
2020-11-05 18:32:12 +0530

    quiz component

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,3 +1,4 @@
+import { QuizapiService } from './../quizapi.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizComponent implements OnInit {
 
-  constructor() { }
+  constructor( private quizdata:QuizapiService) { }
+  public quiz:any;
+  public questions:any;
 
   ngOnInit(): void {
-  }
+    this.quizdata.getquiz().subscribe(data=>{
+      console.log(data);
+      this.quiz=data;
+      console.log(typeof(this.quiz));
+      // this.questions=this.quiz.resul
+  });
 
+}
 }
