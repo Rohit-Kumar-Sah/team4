fcd233791a8a504857bf1e72ad10c70666256205
Rupam Mondal <mondalrupam2014@gmail.com>
2020-11-12 17:44:25 +0530

    fixing some changes, redesign

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,7 +1,7 @@
 import { Router } from '@angular/router';
 import { ScoreService } from './score.service';
 import { QuizapiService } from './../quizapi.service';
-import { Component, OnInit,  } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import { WatchlistdataService } from '../watchlistdata.service';
 
 
@@ -13,38 +13,41 @@ import { WatchlistdataService } from '../watchlistdata.service';
 })
 export class QuizComponent implements OnInit {
 
-  constructor(private quizdata: QuizapiService, private scoreservice:ScoreService, private route:Router,private watchlistitems:WatchlistdataService) { }
+  constructor(private quizdata: QuizapiService, private scoreservice: ScoreService, private route: Router, private watchlistitems: WatchlistdataService) { }
   public quiz: any;
   public questions: any;
   public options = [];
   id: number = 0;
-  public selectedanswer:any=[];
-  public  quizanswer:any[]=[];
-  score=0;
-  
+  public selectedanswer: any = [];
+  public quizanswer: any[] = [];
+  score = 0;
+  selectedOption:any;
+ 
+
 
   ngOnInit(): void {
     this.quizdata.getquiz().subscribe((data: any) => {
-      
-      // console.log(data);
+
+     
       this.quiz = data.results;
-     //console.log(this.quiz);
+     
       this.quiz = this.quiz.map(item => {
         item['optionsArray'] = [...item.incorrect_answers]
         item.optionsArray.push(item.correct_answer)
-        //item.optionsArray.sort();
+        item.optionsArray.sort();
         item['Q_id'] = this.id++;
-
-        this.watchlistitems.routing=false;//shuts down reload page;
         
+
+        this.watchlistitems.routing = false;//shuts down reload page;
+
         return item;
       });
-      
+
 
 
 
     });
-   
+
 
 
 
@@ -53,48 +56,44 @@ export class QuizComponent implements OnInit {
 
 
   }
-  
+
 
   Answer(Q_id, choice) {
-     let flag=false;
-    
-    this.selectedanswer.map(item=>{
-      if(item.id==Q_id){
-    //   flag=true;
-    //  console.log(item.id);
-    //  console.log(item);
-    this.selectedanswer.splice(item.id,1);
-    //  this.selectedanswer.push({
-    //   'id':Q_id,
-    //   'selectedchoice':choice
-    // });
-     
+
+
+    this.selectedanswer.map(item => {
+      if (item.id == Q_id) {
+
+        this.selectedanswer.splice(item.id, 1);
+
 
       }
     });
-   
+
     this.selectedanswer.push({
-      'id':Q_id,
-      'selectedchoice':choice
+      'id': Q_id,
+      'selectedchoice': choice
     }
     );
-    console.log(this.selectedanswer);
+    this.selectedOption=choice;
+   
+    
 
   }
-  check(){
-    this.selectedanswer.map(item =>{
-      for(let i=0;i<this.quiz.length;i++){
+  check() {
+    this.selectedanswer.map(item => {
+      for (let i = 0; i < this.quiz.length; i++) {
 
-        if(item.id==this.quiz[i].Q_id){
-          if(item.selectedchoice==this.quiz[i].correct_answer){
+        if (item.id == this.quiz[i].Q_id) {
+          if (item.selectedchoice == this.quiz[i].correct_answer) {
             this.score++;
-           
-            //console.log(this.quiz[i].correct_answer);
+
+
           }
 
         }
 
-        
+
       }
     })
     //console.log(this.score);
@@ -102,10 +101,11 @@ export class QuizComponent implements OnInit {
     this.route.navigate(['/result']);
 
 
-    
 
+
+  
   }
-
+  
 }
 
 
