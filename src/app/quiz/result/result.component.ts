3b6d478e0c0dcbd1b4d6425ef0d973b00d4e2fe5
Rupam Mondal <mondalrupam2014@gmail.com>
2020-11-17 17:30:58 +0530

    little change

--- src/app/quiz/result/result.component.ts
+++ src/app/quiz/result/result.component.ts
@@ -5,21 +5,24 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
-  styleUrls: ['./result.component.css']
+  styleUrls: ['./result.component.css'],
 })
 export class ResultComponent implements OnInit {
+  UrScore: number;
+  isCongo: boolean;
 
- 
-  UrScore:number
-  constructor(private myscore :ScoreService,private route:Router) { }
-  
+  constructor(private myscore: ScoreService, private route: Router) {
+    this.isCongo = false;
+  }
 
   ngOnInit(): void {
-
-    this.myscore.currentScore.subscribe(data=>
-      this.UrScore=data
-      )
+    this.myscore.currentScore.subscribe((data) => {
+      this.UrScore = data;
+      if (this.UrScore > 5) {
+        this.isCongo = true;
+      } else {
+        this.isCongo = false;
+      }
+    });
   }
-  
-
 }
